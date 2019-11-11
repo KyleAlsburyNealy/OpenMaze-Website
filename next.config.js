8b6da73f10c7b71d9c9bba0b0ec314c9b9f9b3bb
AlexGordienko <alexgor99@gmail.com>
2019-11-11 12:59:14 -0500

    fixed ability to add images into components. Added styiling for index

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withCSS = require('@zeit/next-css')
+const withImages = require('next-images')
 
-module.exports = withCSS({
+module.exports = withCSS(withImages({
     webpack(config, options) {
         // https://github.com/tailwindcss/tailwindcss/pull/1058#issuecomment-518878297
         config.node = { fs: 'empty' };
     
         return config;
       },
-})
+}))
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import '../style.css'
 import Layout from '../components/layout'
+import background from '../public/backgroundOverlay.svg'
 
 const Index = () => (
     <>
-        <Layout>
-            <div>
-                <h1>This is the new home page.</h1>
-            </div>
-        </Layout>
+        <div className="h-screen bg-blue-900">
+            <h1 className="text-white px-8 pt-4 font-light">
+                <span className="border-solid border-r border-white pr-2 mr-2 text-3xl">OpenMaze</span>
+                <span className="text-l">Free 3D research software</span>
+                <button className="bg-red-100 text-white py-2 px-8 rounded-full float-right">
+                    Get Started
+                </button>
+            </h1>
+            <img src={background} />
+        </div>
     </>
 );
 
