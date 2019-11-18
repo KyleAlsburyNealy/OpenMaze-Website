a217dda5dd81e90bde0a514298642815957b11d8
AlexGordienko <alexgor99@gmail.com>
2019-11-18 16:09:13 -0500

    info card is now mobile compatible

--- components/InfoCard/info-card-desktop.js
+++ components/InfoCard/info-card-desktop.js
@@ -6,13 +6,13 @@ class InfoCard extends Component {
     render() {
         return (
             <>
-                <div className="flex items-center h-auto w-10/12 float-right">
-                    <div className="w-2/5 h-full align-middle -mr-24">
+                <div className="lg:flex lg:items-center h-auto lg:w-10/12 lg:float-right md:justify-center flex-wrap md:m-12 sm:m-4 m-4 lg:m-0 xl:m-0">
+                    <div className="lg:w-2/5 h-full lg:align-middle lg:-mr-24 md:mx-16 -mb-12 lg:mb-0">
                         <ReactPlayer url={this.props.yt} width='100%' />
                     </div>
-                    <div className="w-3/5 bg-white flex items-center h-full">
-                        <div className='w-full'>
-                        <div className="mx-32 my-24">
+                    <div className="lg:w-3/5 bg-white flex lg:items-center h-full md:w-full">
+                        <div className='md:w-full'>
+                        <div className="md:mx-32 my-24 mx-12">
                             <div>
                                 <p className="text-2xl font-bold"><span className="text-red-100">&#9679; </span>{this.props.title}</p>
                             </div>
