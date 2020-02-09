4706126d615a8bfb93c051e694eb5d7cef94ea52
AlexGordienko <alexgor99@gmail.com>
2020-02-09 17:41:04 -0500

    finished all additional table data and improved docs page mobile compatibility

--- pages/documentation.js
+++ pages/documentation.js
@@ -1,6 +1,7 @@
 import Layout from "../components/layout";
 import DocumentSection from "../components/documentSection";
 import DocsTable from "../components/docsTable";
+import ReactPlayer from 'react-player'
 
 const sectionLinkStyle = "block py-2 text-lg";
 
@@ -70,6 +71,9 @@ export default function Documentation() {
               <a href="#goals" className={sectionLinkStyle}>
                 Goals & Landmarks
               </a>
+              <a href="#enclosures" className={sectionLinkStyle}>
+                Enclosures
+              </a>
 
               <p className="opacity-75 mt-4 md:mb-2 md:mt-8 text-sm">Tutorials</p>
               <hr className="opacity-75 w-full mb-2 md:hidden" />
@@ -127,7 +131,7 @@ export default function Documentation() {
               <p>
                 Blocks allow for organization and clear visualization of trial data. The order of blocks can be changed within the configuration file, which allows for easy manipulation of trial order when designing an experiment.
               </p>
-              <div className="border border-white rounded-lg my-8">
+              <div className="border border-white rounded-lg my-8 overflow-x-auto">
               <table className="table-auto">
                 <thead className="border-b">
                   <tr>
@@ -190,7 +194,7 @@ export default function Documentation() {
               <p>
               This section of the configuration file allows for organization of information within trials. This includes parameters that create a 3D environment tailored to your task, selection among different goal types and the timing of your trials.
                </p>
-              <div className="border border-white rounded-lg my-8">
+              <div className="border border-white rounded-lg my-8 overflow-x-auto">
               <table className="table-auto">
                 <thead className="border-b">
                   <tr>
@@ -317,7 +321,209 @@ export default function Documentation() {
               </div>
             </div>
 
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24" id="goals">
+              <h1 className="font-medium text-3xl my-4">Goals and Landmarks</h1>
+              <p>
+              In this section you can define different aspects of your goal objects. From physical characteristics such as size and colour to others including location and sound. You can create a wide variety of goal objects of varying salience to suit your task. You can also define the characteristics of your landmarks. This includes parameters that regulate location, size and colour. With the ability to manipulate features of landmarks you can further define the environment to reach your desired goal.
+                </p>
+              <div className="border border-white rounded-lg my-8 overflow-x-auto">
+              <table className="table-auto">
+                <thead className="border-b">
+                  <tr>
+                    <th class="p-4">Parameter Name</th>
+                    <th class="p-4">Variable Description</th>
+                    <th class="p-4">Default</th>
+                    <th class="p-4">Function Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td class=" px-4 py-2">Goals/Landmarks</td>
+                    <td class=" px-4 py-2">List of Goals/Landmarks</td>
+                    <td class=" px-4 py-2">[{}]</td>
+                    <td class=" px-4 py-2">Ordered list of Goals/Landmarks. Indices are used in when defining 3D Trials. Indexing begin at 1</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Goal#/
+Landmark#
+</td>
+                    <td class=" px-4 py-2">String</td>
+                    <td class=" px-4 py-2">None</td>
+                    <td class=" px-4 py-2">Faux parameter that can be used by experimenters to keep track of indices and describe the Goal or Landmark.</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Type</td>
+                    <td class=" px-4 py-2">“3D” or “2D”</td>
+                    <td class=" px-4 py-2">None</td>
+                    <td class=" px-4 py-2">Defines whether the object being defined is a 3D object or a 2D image.</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Object</td>
+                    <td class=" px-4 py-2">File Name</td>
+                    <td class=" px-4 py-2">None</td>
+                    <td class=" px-4 py-2">Refers to the file name of the object in the “3D_Objects” or “2D_Objects” folder or prescribed subfolder.</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Instructional</td>
+                    <td class=" px-4 py-2">Binary (0 or 1)</td>
+                    <td class=" px-4 py-2">0</td>
+                    <td class=" px-4 py-2">Must be set to 1 when creating an instructional trial. Can be excluded when creating a 3D trial</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Color</td>
+                    <td class=" px-4 py-2">RGB or HTML color code</td>
+                    <td class=" px-4 py-2">“”</td>
+                    <td class=" px-4 py-2">Sets the color of the object when using a 3D object</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Position</td>
+                    <td class=" px-4 py-2">[x,y,z]</td>
+                    <td class=" px-4 py-2">[0,0,0]</td>
+                    <td class=" px-4 py-2">Position the goal will be placed within the environment. Corresponds to [x,y,z] Position parameters in Unity Inspector window</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Rotation</td>
+                    <td class=" px-4 py-2">[x,y,z]</td>
+                    <td class=" px-4 py-2">[0,0,0]</td>
+                    <td class=" px-4 py-2">Orientation the goal will be placed within the environment. Corresponds to [x,y,z] Rotation parameters in Unity Inspector window</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Scale</td>
+                    <td class=" px-4 py-2">[x,y,z]</td>
+                    <td class=" px-4 py-2">[0,0,0]</td>
+                    <td class=" px-4 py-2">Size of the goal. Corresponds to [x,y,z] Scale parameters in Unity Inspector window</td>
+                  </tr>
+                  <tr class="bg-dark-light border-white">
+                    <td class=" px-4 py-2">GOALS ONLY</td>
+                    <td class=" px-4 py-2"></td>
+                    <td class=" px-4 py-2"></td>
+                    <td class=" px-4 py-2"></td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Sound</td>
+                    <td class=" px-4 py-2">File Name</td>
+                    <td class=" px-4 py-2">None</td>
+                    <td class=" px-4 py-2">Sound to be played upon collision when goal is called as ActiveGoal or InvisibleGoal </td>
+                  </tr>
+                </tbody>
+              </table>
+              </div>
+            </div>
 
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24" id="enclosures">
+              <h1 className="font-medium text-3xl my-4">Enclosures</h1>
+              <p>
+              In this section you can define different aspects of your goal objects. From physical characteristics such as size and colour to others including location and sound. You can create a wide variety of goal objects of varying salience to suit your task. You can also define the characteristics of your landmarks. This includes parameters that regulate location, size and colour. With the ability to manipulate features of landmarks you can further define the environment to reach your desired goal.
+                </p>
+              <div className="border border-white rounded-lg my-8 overflow-x-auto">
+              <table className="table-auto">
+                <thead className="border-b">
+                  <tr>
+                    <th class="p-4">Parameter Name</th>
+                    <th class="p-4">Variable Description</th>
+                    <th class="p-4">Default</th>
+                    <th class="p-4">Function Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Enclosures</td>
+                    <td class=" px-4 py-2">List of Enclosures</td>
+                    <td class=" px-4 py-2">[{}]</td>
+                    <td class=" px-4 py-2">Ordered list of Enclosures. Indices are used in when defining 3D Trials. Indexing begin at 1</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Enclosure#</td>
+                    <td class=" px-4 py-2">string</td>
+                    <td class=" px-4 py-2">None</td>
+                    <td class=" px-4 py-2">Faux parameter that can be used by experimenters to track indices and describe the enclosure.</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">Sides</td>
+                    <td class=" px-4 py-2"># of Enclosure walls</td>
+                    <td class=" px-4 py-2">0</td>
+                    <td class=" px-4 py-2">Number of sides of the enclosure (ex. 4 = square, 5 = pentagon, 6 = hexagon, etc.).</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">Radius</td>
+                    <td class=" px-4 py-2">Float variable</td>
+                    <td class=" px-4 py-2">0</td>
+                    <td class=" px-4 py-2">Used to set the size of the enclosure. Walls are created tangent to the circle defined by the radius</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">WallHeight</td>
+                    <td class=" px-4 py-2">Float variable</td>
+                    <td class=" px-4 py-2">0</td>
+                    <td class=" px-4 py-2">Sets the height of the Enclosure walls</td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">WallColor</td>
+                    <td class=" px-4 py-2">HTML or RGB color code</td>
+                    <td class=" px-4 py-2">“ffffff”</td>
+                    <td class=" px-4 py-2">Sets color of enclosure walls.</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">GroundTileSides</td>
+                    <td class=" px-4 py-2">Sides per tile</td>
+                    <td class=" px-4 py-2">0</td>
+                    <td class=" px-4 py-2">Works similar to “Sides” parameter. Allows experimenters to customize the floor of the enclosure </td>
+                  </tr>
+                  <tr>
+                    <td class=" px-4 py-2">GroundTileSize</td>
+                    <td class=" px-4 py-2">Size of Tiles </td>
+                    <td class=" px-4 py-2">1</td>
+                    <td class=" px-4 py-2">A number between 0-1 which manipulates the size of each ground tile</td>
+                  </tr>
+                  <tr class="bg-dark-light">
+                    <td class=" px-4 py-2">GroundColor</td>
+                    <td class=" px-4 py-2">HTML or RGB color code</td>
+                    <td class=" px-4 py-2">“ffffff”</td>
+                    <td class=" px-4 py-2">Sets color of ground tiles. </td>
+                  </tr>
+
+                </tbody>
+              </table>
+              </div>
+            </div>
+
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="basicsTutorial">
+              <p className="font-sm font-hairline opacity-75">Tutorials</p>
+              <h1 className="font-medium text-3xl my-4">3D Trial Basics</h1>
+              <ReactPlayer url={"https://youtu.be/CnlVgKkj3C4"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="trialObjectsTutorial">
+              <h1 className="font-medium text-3xl my-4">3D Trial Objects</h1>
+              <ReactPlayer url={"https://youtu.be/sSOWmIR7dWA"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="buildingEnvironmentsTutorial">
+              <h1 className="font-medium text-3xl my-4">Building Environments</h1>
+              <ReactPlayer url={"https://youtu.be/rC0S63hHopo"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="blocksTutorial">
+              <h1 className="font-medium text-3xl my-4">Blocks</h1>
+              <ReactPlayer url={"https://youtu.be/Nqj0VUgi4gE"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="goalObjectsTutorial">
+              <h1 className="font-medium text-3xl my-4">Goal Objects</h1>
+              <ReactPlayer url={"https://youtu.be/20UysrQwdgU"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="instructionTrialsTutorial">
+              <h1 className="font-medium text-3xl my-4">Instruction Trials</h1>
+              <ReactPlayer url={"https://youtu.be/64lVb1U0KIg"} width='100%' />
+            </div>
+
+            <div className="text-white text-left max-w-sm md:max-w-lg lg:max-w-2xl mb-24 w-full" id="enclosuresTutorial">
+              <h1 className="font-medium text-3xl my-4">Enclosures</h1>
+              <ReactPlayer url={"https://youtu.be/DTH-Bc1m_wc"} width='100%' />
+            </div>
+
+            
           </div>
         </div>
       </Layout>
