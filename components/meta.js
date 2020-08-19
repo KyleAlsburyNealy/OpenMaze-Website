46e0b5ccb5993def37eef5392bd8c90f8c363a59
kbnealy <k.b.nealy@gmail.com>
2020-08-19 15:01:18 -0400

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -6,7 +6,7 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <meta property="og:title" content="OpenMaze Experiment Design Toolbox" />
+      <meta property="og:title" content="OpenMaze 3D Experiment Design Toolbox" />
       <meta property="og:image" content="https://lh3.googleusercontent.com/GIQnzZ3qHuXp3nkuJNubBxpC9ue64yAYMpE5FEbrw6YLSrU0-wrJe4TcUvjFxk5L3KSV5nP8_87FTz7kTUSUDt421TvsANXjeVittK-_S1V-6t6omcIwYEPy1s8Yf65TyNcnP3ejOAw=w2400" />
       <meta property="og:description" content="OpenMaze is a general-purpose 3D experiment building toolbox, developed for use with Unity Software. OpenMaze requires no prior coding experience, allowing you to spend less time hacking and more time experimenting!"/>
       <meta property="og:url" content="https://openmaze.duncanlab.org/"/>  
