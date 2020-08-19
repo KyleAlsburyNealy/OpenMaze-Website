a765ef010a7e7bcf206d10921c63f87f498504c4
kbnealy <k.b.nealy@gmail.com>
2020-08-19 14:28:21 -0400

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-
 // The purpose of this page to set some default styles for the whole application.
 // Place things like background color and font here.
 export default () => (
@@ -7,6 +6,12 @@ export default () => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta property="og:title" content="OpenMaze Experiment Design Toolbox" />
+      <meta property="og:image" content="https://openmaze.duncanlab.org/_next/static/images/WebsiteLogoFinal-71d999db7d668b611c761e3a8ae9e388.png" />
+      <meta property="og:description" content="OpenMaze is a general-purpose 3D experiment building toolbox, developed for use with Unity Software. OpenMaze requires no prior coding experience, allowing you to spend less time hacking and more time experimenting!"/>
+      <meta property="og:url" content="https://openmaze.duncanlab.org/"/>  
+      <title>OpenMaze</title>
+ 
       <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
       <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
